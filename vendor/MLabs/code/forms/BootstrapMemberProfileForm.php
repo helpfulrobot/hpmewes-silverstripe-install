05fce1255f8ed03f3aee3bb5661cee7462a18d6e
© MLabs <meweshp@googlemail.com>
2015-01-19 02:31:36 +0100

    Create BootstrapMemberProfileForm.php

---
+++ vendor/MLabs/code/forms/BootstrapMemberProfileForm.php
@@ -0,0 +1,22 @@
+<?php
+class BootstrapMemberProfileForm extends Extension {
+    
+    public function updateProfileForm(Form $form) {
+        if(!Config::inst()->get('BootstrapForm', 'bootstrap_included')) {
+            Requirements::css(BOOTSTRAP_FORMS_DIR.'/css/bootstrap.css');
+        }
+        if(!Config::inst()->get('BootstrapForm', 'jquery_included')) {
+            Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
+        }
+        if(!Config::inst()->get('BootstrapForm', 'bootstrap_form_included')) {
+       	Requirements::javascript(BOOTSTRAP_FORMS_DIR."/javascript/bootstrap_forms.js");
+        }        
+        $form->Fields()->bootstrapify();
+        $form->Actions()->bootstrapify();
+        $form->addExtraClass('well');
+        $form->addExtraClass('form-horizontal');
+        $form->setTemplate('BootstrapForm');
+    } 
+
+}
+?>
