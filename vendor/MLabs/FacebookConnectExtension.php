1c69bf637d19d5b08c48fc86293511a2568cb175
© MLabs <meweshp@googlemail.com>
2015-04-02 08:48:14 +0200

    move facebook setting
    moved from Installer

--- vendor/MLabs/FacebookConnectExtension.php
+++ vendor/MLabs/FacebookConnectExtension.php
@@ -3,11 +3,21 @@ namespace MLabs;
 
 class FacebookConnectExtension extends Extension {
 
+    // register your website / app https://developers.facebook.com/apps/?action=create
+    private static $facebook_app_id     = "[app_id]";       // app id of facebook app
+    private static $facebook_api_secret = "[api_secret]";   // app secret of facebook app
+    
+    public static function getFacebookAppId() { return self::$facebook_app_id; }
+    public static function getFacebookApiSecret() { return self::$facebook_api_secret; }
+    
     public static function add() {
         parent::add();
         
-        File::replaceContent(Installer::getRootDirConfig().self::getConfigfile(), "[app_id]", Installer::getFacebookAppId());
-        File::replaceContent(Installer::getRootDirConfig().self::getConfigfile(), "[api_secret]", Installer::getFacebookApiSecret());
+        self::$facebook_app_id = Installer::getComposerEvent()->getIO()->ask(":: enter your facebook app id: ", "[app_id]");
+        self::$facebook_api_secret = Installer::getComposerEvent()->getIO()->ask(":: enter your facebook app secret: ", "[app_secret]");
+        
+        File::replaceContent(Installer::getRootDirConfig().self::getConfigfile(), "[app_id]", self::getFacebookAppId());
+        File::replaceContent(Installer::getRootDirConfig().self::getConfigfile(), "[api_secret]", self::getFacebookApiSecret());
         File::addContent(Installer::getRootDirTheme()."Includes/Footer.ss", "<% include FacebookLoginLink %>", "SilverStripe</a></small>");
         
         Installer::getComposerEvent()->getIO()->write(":: added facebookconnect extension");
